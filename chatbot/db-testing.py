a44a858d770a9f81e74dbfd6f94940c790993b8d
John <jnjporter19@gmail.com>
2024-06-17 16:05:10 -0400

    move directory and update

---
+++ chatbot/db-testing.py
@@ -0,0 +1,20 @@
+import chromadb
+import streamlit as st 
+import sys
+sys.path.append('b:\\code\\projects\\chatbot\\pages')
+from admin_2 import create_collection, delete_collection, change_name, db_collections
+
+if 'col_choice' not in st.session_state:
+    st.session_state['col_choice']=None
+st.session_state
+
+run_client = chromadb.PersistentClient("b:\\python\\database\\booboo")
+try:
+    run_client.get_collection(name="test")
+except Exception as err:
+    st.write(str(err))
+
+create_collection(run_client,"john")
+st.selectbox("Pick a Collection",db_collections(run_client),key='col_picker',index=None)
+st.write(st.session_state.col_picker)
+
